ead34aaa09d24c674fdd0b84c5651c4ef464aaec
Stas Sribnyi <stas.sribnyi@gmail.com>
2019-08-06 17:22:51 +0300

    Performance improvement (#1)
    * Replace heavy animations with lighter ones
    
    * Replace Waypoint.js with Scrollama.js library to improve performance
    
    * Restore the previous zoom like effect for loaded images
    
    * Decrease section offset for link highlighting
    
    * Fix noscript image formatting

--- static/js/libs.js
+++ static/js/libs.js
@@ -1,6 +1,6 @@
 export const lozad = window.lozad;
 export const Glide = window.Glide;
-export const Waypoint = window.Waypoint;
+export const scrollama = window.scrollama;
 export const SweetScroll = window.SweetScroll;
 
 export const GLIDE_CONFIG_DEFAULT = {
@@ -13,13 +13,17 @@ export const GLIDE_CONFIG_DEFAULT = {
   animationTimingFunc: "cubic-bezier(0.6, 0, 0.14, 1)"
 };
 
-export const SWEET_SCROLL_CONFIG_DEFAULT ={
+export const SWEET_SCROLL_CONFIG_DEFAULT = {
   offset: 1,
-  duration: 3500,
-  easing: "easeOutQuint"
+  easing: "easeInOutSine"
 };
 
-export const Direction = Object.freeze({
+export const MoveDirection = Object.freeze({
   UP: "up",
   DOWN: "down"
+});
+
+export const StepDirection = Object.freeze({
+  ENTER: Symbol("enter"),
+  EXIT: Symbol("exit")
 });
