b148f20b998167e000129d39cf4dec67af0cbd2e
Stas Sribnyi <stas.sribnyi@gmail.com>
2023-08-17 21:29:43 +0300

    Improve about section

---
+++ src/sections/About/About.data.ts
@@ -0,0 +1,15 @@
+export const ME = {
+    photoUrl: '/img/profiles/about.jpg',
+    firstName: 'Stas',
+    lastName: 'Sribnyi',
+    location: 'Europe, Ukraine, Kyiv',
+    age: 26,
+    description: `I am passionate about working with UI and I use the latest technologies 
+    such as React and Angular (although I personally prefer using React and Vanilla Javascript). 
+    I have experience working as a full-stack engineer in the past, so I am familiar with all key parts required to build excellent application. 
+    Currently, I am focused on UI development and applying all my knowledge in this field. 
+    I started my career more than five years ago by studying at university and simultaneously visiting full-stack software engineering courses. 
+    Since then, I have worked with several companies,  held different positions, participated
+    in vast variety of projects, and more importantly gained valuable experience that I would love to share with you.`,
+    cvUrl: 'https://drive.google.com/open?id=0B4WwhDkyLxKpSDZzdzU4YUdBX1k',
+}
--- src/sections/About/About.tsx
+++ src/sections/About/About.tsx
@@ -11,22 +11,7 @@ import {
   Box,
 } from '@mui/material'
 
-const ME = {
-  photoUrl: '/img/profiles/about.jpg',
-  firstName: 'Stas',
-  lastName: 'Sribnyi',
-  location: 'Europe, Ukraine, Kyiv',
-  age: 26,
-  description: `I am passionate about working with UI and I use the latest technologies 
-  such as React and Angular (although I personally prefer using React and Vanilla Javascript). 
-  I have experience working as a full-stack engineer in the past, so I am familiar with all key parts required to build excellent application. 
-  Currently, I am focused on UI development and applying all my knowledge in this field. 
-  I started my career more than five years ago by studying at university and simultaneously visiting full-stack software engineering courses. 
-  Since then, I have worked with several companies,  held different positions, participated
-  in vast variety of projects, and more importantly gained valuable experience that I would love to share with you.`,
-  cvUrl: 'https://drive.google.com/open?id=0B4WwhDkyLxKpSDZzdzU4YUdBX1k',
-}
-
+import { ME } from './About.data'
 export const About: React.FC = () => {
   const { age, cvUrl, description, firstName, lastName, location, photoUrl } =
     ME
@@ -41,49 +26,55 @@ export const About: React.FC = () => {
 
   return (
     <Section id="about" title="Who am I?" sx={{ bgcolor: 'common.light' }}>
-      <Stack direction="row" gap={20} sx={{ margin: '4rem auto' }}>
-        <Stack alignItems="center">
-          <Typography
-            gutterBottom
-            align="center"
-            component="h3"
-            variant="h4"
-            color="secondary"
-          >
-            Software development without compromising on robustness
-          </Typography>
-          <Typography
-            variant="body1"
-            sx={{
-              lineHeight: 1.65,
-              textIndent: '2em',
-              marginBottom: '2rem',
-            }}
-          >
-            {description}
-          </Typography>
+      <Grid container spacing={8}>
+        <Grid xs={12} md={5} lg={4}>
+            <Avatar
+              src={photoUrl}
+              alt={displayName}
+              variant="rounded"
+              sx={{
+                width: '100%',
+                height: '100%',
+                transform: 'rotateY(180deg)',
+                // TODO: Move into theme configuration
+                boxShadow: '0px 2px 2px 0px rgba(var(--dark, 51, 51, 51), 0.4)',
+              }}
+            />
+        </Grid>
 
-          <Button href={cvUrl} target="_blank" rel="noreferrer">
-            Want to print my CV?
-          </Button>
-        </Stack>
+        <Grid xs={12} md={7} lg={8}>
+          <Stack alignItems="start">
+            <Typography
+              gutterBottom
+              align="center"
+              component="h3"
+              variant="h4"
+              color="secondary"
+            >
+              Software development without compromising on robustness
+            </Typography>
+            <Typography
+              variant="body1"
+              align='justify'
+              sx={{
+                lineHeight: 1.65,
+                textIndent: '2em',
+                marginBottom: '2rem',
+              }}
+            >
+              {description}
+            </Typography>
 
-        <Avatar
-          src={photoUrl}
-          alt={displayName}
-          variant="rounded"
-          sx={{
-            width: 345,
-            height: 570,
+            <Button href={cvUrl} target="_blank" rel="noreferrer">
+              Want to print my CV?
+            </Button>
+          </Stack>
+        </Grid>
 
-            // TODO: Move into theme configuration
-            boxShadow: '0px 2px 2px 0px rgba(var(--dark, 51, 51, 51), 0.4)',
-          }}
-        />
         {/* <Typography component='figcaption'>
             <Details items={details} />
           </Typography> */}
-      </Stack>
+      </Grid>
     </Section>
   )
 }
